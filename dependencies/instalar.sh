1f3c503378a13a4150ab84a59e8a5fdffc2eecfc
Cavallinux <pmezzano@gmail.com>
2016-05-12 19:24:33 -0300

    MOD
    Se agrega fuentes de las dependencias manuales del proyecto

--- dependencies/instalar.sh
+++ dependencies/instalar.sh
@@ -2,9 +2,9 @@
 
 export MVN_HOME=~cavallinux/dev/bin/maven/3.2.3
 
-$MVN_HOME/bin/mvn install:install-file -Dfile=org.eclipse.swt/org.eclipse.swt-linux-x86_64.jar -DgroupId=org.eclipse.swt -DartifactId=swt-linux-x86_64 -Dversion=3.4.0 -Dpackaging=jar
+$MVN_HOME/bin/mvn install:install-file -Dfile=org.eclipse.swt/org.eclipse.swt-linux-x86_64.jar -DgroupId=org.eclipse.swt -DartifactId=swt-linux-x86_64 -Dversion=3.4.0 -Dsources=../dependencies-sources/org.eclipse.swt/org.eclipse.swt.zip -Dpackaging=jar
 
-$MVN_HOME/bin/mvn install:install-file -Dfile=org.eclipse.jface/org.eclipse.jface.jar -DgroupId=org.eclipse.jface -DartifactId=jface -Dversion=3.4.0 -Dpackaging=jar
+$MVN_HOME/bin/mvn install:install-file -Dfile=org.eclipse.jface/org.eclipse.jface.jar -DgroupId=org.eclipse.jface -DartifactId=jface -Dversion=3.4.0 -Dsources=../dependencies-sources/org.eclipse.jface/org.eclipse.jface.jar -Dpackaging=jar
 
 $MVN_HOME/bin/mvn install:install-file -Dfile=org.eclipse.jface/org.eclipse.core.commands.jar -DgroupId=org.eclipse.core -DartifactId=commands -Dversion=3.4.0 -Dpackaging=jar
 

950a42975d26cb45a2df258fe1d1a5990f5098d9
Paolo Mezzano Barahona <pmezzano@gmail.com>
2026-04-14 15:32:43 -0400

    MOD
    Closes #13

---
+++ jisocreator-action/src/main/java/cl/cavallinux/jisocreator/gui/decl/ICompositeCreator.java
@@ -0,0 +1,9 @@
+package cl.cavallinux.jisocreator.gui.decl;
+
+public interface ICompositeCreator {
+    public void createComponents();
+
+    public void addFeatures();
+
+    public void addListeners();
+}

6afcb86b8c874e577dbb7cc13f8246e83d0a0bb0
Paolo Mezzano Barahona <pmezzano@gmail.com>
2026-04-18 13:21:55 -0400

    MOD
    Refactor de clases filtro y comparators

--- jisocreator-model/src/main/java/cl/cavallinux/jisocreator/model/comparators/OSDirectoriesComparator.java
+++ jisocreator-model/src/main/java/cl/cavallinux/jisocreator/model/comparators/OSDirectoriesComparator.java
@@ -1,12 +1,21 @@
 package cl.cavallinux.jisocreator.model.comparators;
 
 import java.io.File;
+import java.math.BigInteger;
 
 import org.eclipse.jface.viewers.ViewerComparator;
 
+/**
+ * Comparator para directorios nativos del sistema operativo
+ * 
+ * @author Paolo Mezzano Barahona (pmezzano@gmail.com)
+ * @version 0.0.2
+ * @since 0.0.2
+ */
 public class OSDirectoriesComparator extends ViewerComparator {
     @Override
     public int category(Object element) {
-        return ((File) element).isDirectory() ? 0 : 1;
+        File file = (File) element;
+        return file.isDirectory() ? BigInteger.ZERO.intValue() : BigInteger.ONE.intValue();
     }
 }
--- jisocreator-model/src/main/java/cl/cavallinux/jisocreator/model/filters/HideHiddenFilesFilter.java
+++ jisocreator-model/src/main/java/cl/cavallinux/jisocreator/model/filters/HideHiddenFilesFilter.java
@@ -5,11 +5,18 @@ import java.io.File;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 
+/**
+ * Filtro para mostrar u ocultar los archivos ocultos del sistema operativo
+ * 
+ * @author Paolo Mezzano Barahona (pmezzano@gmail.com)
+ * @version 0.0.2
+ * @since 0.0.2
+ */
 public class HideHiddenFilesFilter extends ViewerFilter {
 
     @Override
     public boolean select(Viewer arg0, Object arg1, Object arg2) {
-        return !((File) arg2).isHidden();
+        File file = (File) arg2;
+        return !file.isHidden();
     }
-
 }

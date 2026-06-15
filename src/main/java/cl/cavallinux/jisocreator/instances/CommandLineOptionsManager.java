a35f7cb179dea41b9340c363dbe5a4a263d72d62
Paolo Mezzano Barahona <pmezzano@gmail.com>
2026-06-14 23:58:51 -0400

    feat: add environment variable to setup logs path and changed command line parsing logic

--- src/main/java/cl/cavallinux/jisocreator/instances/CommandLineOptionsManager.java
+++ src/main/java/cl/cavallinux/jisocreator/instances/CommandLineOptionsManager.java
@@ -8,12 +8,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CommandLineOptionsManager {
+    LOAD(Option.builder("l").longOpt("load").desc("Load existing xml file into GUI").hasArg().argName("xmllayout")
+            .build()),
     HELP(Option.builder("h").longOpt("help").desc("Show this help message").build()),
-    LOAD(Option.builder("l").longOpt("load").desc("Load existing xml file into GUI").hasArg().build()),
-    DEBUG(Option.builder("d").longOpt("debug").desc("Start debug mode").build()),
-    ISOINPUT(Option.builder("i").longOpt("input").desc("ISO XML Layout to load").required(false).hasArg().build()),
-    ISOOUTPUT(Option.builder("o").longOpt("output").desc("ISO File output path").required(false).hasArg().build()),
-    VERSION(Option.builder("v").longOpt("version").desc("Show application version and jvm version").build());
+    VERSION(Option.builder("v").longOpt("version").desc("Show application version and jvm version").build()),
+    ISOINPUT(Option.builder("i").longOpt("input").desc("ISO XML Layout to load").hasArg().argName("xmllayout").build()),
+    ISOOUTPUT(Option.builder("o").longOpt("output").desc("ISO File output path").hasArg().argName("isoFile").build());
 
     private Option option;
 }

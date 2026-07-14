35a9f33e214179af179bd5f19efcdf5d02cd4df5
Paolo Mezzano Barahona <pmezzano@gmail.com>
2026-07-14 12:07:05 -0400

    feat:
    Added logs folder and refactor startup to windows executable

--- res/mkisofs/jisocreator.bat
+++ res/mkisofs/jisocreator.bat
@@ -1,1 +1,3 @@
-java --add-opens java.base/java.util=ALL-UNNAMED --enable-native-access=ALL-UNNAMED -jar jisocreator.jar %*
+set "JISOCREATOR_LOGS=%LOCALAPPDATA%\jisocreator\logs"
+if not exist "%JISOCREATOR_LOGS%" mkdir "%JISOCREATOR_LOGS%"
+start /b javaw -Dpath.logs="%JISOCREATOR_LOGS%" --add-opens java.base/java.util=ALL-UNNAMED --enable-native-access=ALL-UNNAMED -jar jisocreator.jar %*

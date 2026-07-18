ab7ece7210c9b3bfdf89a6dca58808c879c2913e
Paolo Mezzano Barahona <pmezzano@gmail.com>
2026-07-18 00:44:34 -0400

    feat:
    Added JAVA_HOME variable to win32 startup script

--- res/mkisofs/jisocreator.bat
+++ res/mkisofs/jisocreator.bat
@@ -1,3 +1,4 @@
 set "JISOCREATOR_LOGS=%LOCALAPPDATA%\jisocreator\logs"
+set "JAVA_HOME=%ProgramFiles%\jisocreator\jre"
 if not exist "%JISOCREATOR_LOGS%" mkdir "%JISOCREATOR_LOGS%"
-start /b javaw -Dpath.logs="%JISOCREATOR_LOGS%" --add-opens java.base/java.util=ALL-UNNAMED --enable-native-access=ALL-UNNAMED -jar jisocreator.jar %*
+start /b "%JAVA_HOME%"\bin\javaw -Dpath.logs="%JISOCREATOR_LOGS%" --add-opens java.base/java.util=ALL-UNNAMED --enable-native-access=ALL-UNNAMED -jar jisocreator.jar %*
